c986a843d926f051180fd4bca0df6fc4e36c14f4
yal <98389358+EchoXigua@users.noreply.github.com>
2022-11-16 17:06:04 +0800

    单循环链表处理约瑟夫问题-doing

--- 链表/单链表/6.单向循环链表-约瑟夫问题.js
+++ 链表/单链表/6.单向循环链表-约瑟夫问题.js
@@ -1,12 +1,59 @@
 
+//小男孩
+class Boy {
+  no
+  next
+  constructor(no) {
+    this.no = no
+    this.next = null
+  }
+}
 
-/**
- *  1. 先生成单循环链表
- */
 
+class CircleLinkedList {
+  first = new Boy(-1)
 
+  /**
+   *  1. 先生成单循环链表
+   */
+  //创建循环链表
+  addBoy(num) {
+    if (num < 1) {
+      return
+    }
 
+    //通过循环来创建
+    let curBoy = null
+    for (let i = 1; i <= num; i++) {
+      //当创建一个的时候，需要自我循环
+      let boy = new Boy(i)
 
-/**
- *  2. 遍历单循环链表
- */
+      if (i == 1) {
+        this.first = boy
+        this.first.next = boy
+        curBoy = boy
+      } else {
+        curBoy.next = boy
+        boy.next = this.first
+        curBoy = boy //curBoy 后移
+      }
+    }
+  }
+
+  /**
+   *  2. 遍历单循环链表
+   */
+  view() {
+    //需要一个辅助变量来遍历
+    let temp = this.first
+    while (true) {
+      if (temp.next == this.first) {
+        //下一个节点指向头，说明下一个节点是尾节点
+        console.log('boy 编号为：', temp.no);
+        break
+      }
+      console.log('boy 编号为：', temp.no);
+      temp = temp.next  //后移
+    }
+  }
+}
