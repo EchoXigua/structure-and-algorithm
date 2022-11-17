cef7907d3a055a5df35984a064db64f5719ffed3
yal <98389358+EchoXigua@users.noreply.github.com>
2022-11-17 15:45:26 +0800

    约瑟夫问题完成

--- 链表/单链表/6.单向循环链表-约瑟夫问题.js
+++ 链表/单链表/6.单向循环链表-约瑟夫问题.js
@@ -38,6 +38,7 @@ class CircleLinkedList {
         curBoy = boy //curBoy 后移
       }
     }
+    this.view()
   }
 
   /**
@@ -47,13 +48,59 @@ class CircleLinkedList {
     //需要一个辅助变量来遍历
     let temp = this.first
     while (true) {
+      console.log('boy 编号为：', temp.no);
       if (temp.next == this.first) {
         //下一个节点指向头，说明下一个节点是尾节点
-        console.log('boy 编号为：', temp.no);
         break
       }
-      console.log('boy 编号为：', temp.no);
       temp = temp.next  //后移
     }
+  }
+/**
+ * 
+ * @param {*} k 从第几个人开始 
+ * @param {*} m 数几次
+ * @param {*} n 总共多少人
+ */
+  handle(k,m,n){
+    if(k<1 || m<1 || n<1){
+      console.log('参数错误');
+      return
+    }
+
+    this.addBoy(n)  //创建了n个单向环形链表
+    let first = this.first
+    let ender = this.first
+    //1.先遍历，让ender指向链表最后一个
+    while (true) {
+      if(ender.next == first){
+        break
+      }
+      ender = ender.next
+    }
+
+    //2.让ender 和 first 一起移动到k 第几个人.移动k-1次
+    for(let i=0;i<k-1;i++){
+      first = first.next
+      ender = ender.next
+    }
+
+    while (true) {
+      //只剩最后一个人出列
+      if(ender == first){
+        console.log('最后一个出列的序号为：',first.no);
+        break
+      }
+      //3.数到m的人出列， 数m-1次 ender 和 first一起移动
+      for(let i=0;i<m-1;i++){
+        first = first.next
+        ender = ender.next
+      }
+      //4.将数到m的人移除链表
+      console.log('出列的序号为：',first.no);
+      ender.next = first.next
+      first = first.next //first后移
+    }
+    
   }
 }
